31c2b66fef347893b5093320122d57d3af2f5f46
Everton <egeraldo@student.42sp.org.br>
2024-06-27 15:44:58 -0300

    alterando o nome da pasta

---
+++ modulo_00/ex01/Contact.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egeraldo <egeraldo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 15:04:55 by egeraldo          #+#    #+#             */
+/*   Updated: 2024/06/27 14:46:18 by egeraldo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+std::string	Contact::getFirstName(void) const {
+	return this->_firstName;
+}
+
+std::string	Contact::getLastName(void) const {
+	return this->_lastName;
+}
+
+std::string	Contact::getNickname(void) const {
+	return this->_nickname;
+}
+
+std::string	Contact::getPhoneNumber(void) const {
+	return this->_phoneNumber;
+}
+
+std::string	Contact::getDarkestSecret(void) const {
+	return this->_darkestSecret;
+}
+
+void Contact::setFirstName(const std::string& firstName) {
+	this->_firstName = firstName;
+}
+
+void Contact::setLastName(const std::string& lastName) {
+	this->_lastName = lastName;
+}
+
+void Contact::setNickname(const std::string& nickname) {
+	this->_nickname = nickname;
+}
+
+void Contact::setPhoneNumber(const std::string& phoneNumber) {
+	this->_phoneNumber = phoneNumber;
+}
+
+void Contact::setDarkestSecret(const std::string& darkestSecret) {
+	this->_darkestSecret = darkestSecret;
+}
