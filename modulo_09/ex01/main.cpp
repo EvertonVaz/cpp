2c5724016813887c1be0dd4e468d0131b2d1d0fc
Everton <egeraldo@student.42sp.org.br>
2024-07-30 17:43:51 -0300

    ex01 modulo 9 feito

--- modulo_09/ex01/main.cpp
+++ modulo_09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etovaz <egeraldo@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 16:48:14 by etovaz            #+#    #+#             */
-/*   Updated: 2024/07/30 17:41:31 by etovaz           ###   ########.fr       */
+/*   Updated: 2024/07/30 17:43:35 by etovaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ int main(int argc, char **argv) {
 		std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
 		return 1;
 	}
+	
 	try {
 		RPN rpn;
-		int result = rpn.evaluate(argv[1]);
-		std::cout << result << std::endl;
+
+		std::cout << rpn.evaluate(argv[1]) << std::endl;
 	} catch (const std::exception &e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return 1;
